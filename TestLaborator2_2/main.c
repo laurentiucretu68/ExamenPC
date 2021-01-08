ad2fbf66452533a51b1451ff11a9d28183800d1b
laurentiucretu68 <37586514+laurentiucretu68@users.noreply.github.com>
2021-01-08 15:24:22 +0200

    Update main.c

--- TestLaborator2_2/main.c
+++ TestLaborator2_2/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+///Definirea structurii produs
 struct produs{
     char nume[30];
     float pret;
@@ -10,17 +11,21 @@ struct produs{
     struct magazin *m;
 };
 
+///Definirea structurii magazin
 struct magazin{
     char nume[30];
     int gps_x;
     int gps_y;
 };
 
+///Definirea unui nod al listei
 struct nod{
     struct produs prod;
     struct nod* urm;
 };
 
+///Adaugarea elementelor vectorului de produse in lista simplu inlantuita
+///Adaugarea elementelor se realizeaza la finalul listei
 void adaugareElementeLista(struct nod **cap,struct produs p){
     struct nod *curent = (*cap);
     if ((*cap)==NULL){
@@ -38,6 +43,7 @@ void adaugareElementeLista(struct nod **cap,struct produs p){
     curent->urm = nou;
 }
 
+///Functie care afiseaza elementele din lista
 void afisareLista(struct nod *cap){
     while (cap!=NULL){
         printf("%s %.2f %d ",cap->prod.nume,cap->prod.pret,cap->prod.nr_magazine);
@@ -48,17 +54,19 @@ void afisareLista(struct nod *cap){
     }
 }
 
+///Functie de citire a produselor din fisier
 void citire(struct produs **p,int *nrProduse,FILE* fin){
     *nrProduse = 0;
     *p =(struct produs*)malloc(sizeof(struct produs));
     char line[500];
-    while (fgets(line,500,fin)){
+    while (fgets(line,500,fin)){ ///Citirea se realizeaza la nivel de linie de fisier
         *p = realloc(*p,((*nrProduse)+1)*sizeof(struct produs));
-        char *campuri = strtok(line," ,");
-        strcpy((*p)[*nrProduse].nume,campuri); campuri = strtok(NULL," ,");
-        (*p)[*nrProduse].pret = atof(campuri); campuri = strtok(NULL," ,");
-        int contor_magazine = 0;
+        char *campuri = strtok(line," ,"); ///Se sparge linia in campurile unui produs cu functia strtok
+        strcpy((*p)[*nrProduse].nume,campuri); campuri = strtok(NULL," ,"); ///Memorarea numelui
+        (*p)[*nrProduse].pret = atof(campuri); campuri = strtok(NULL," ,"); ///Memorarea pretului
+        int contor_magazine = 0; ///Retine numarul de magazine pentru fiecare produs
         (*p)[*nrProduse].m = (struct magazin*)malloc(sizeof(struct magazin));
+        ///Memorarea campurilor magazinelor unui produs
         while (campuri){
             (*p)[*nrProduse].m = realloc((*p)[*nrProduse].m,(contor_magazine+1)*sizeof(struct magazin));
             strcpy((*p)[*nrProduse].m[contor_magazine].nume,campuri); campuri = strtok(NULL," ,");
@@ -72,6 +80,7 @@ void citire(struct produs **p,int *nrProduse,FILE* fin){
     }
 }
 
+///Functie ce afiseaza produsele din vector
 void afisare(struct produs *p,int nrProduse){
     for (int i=0; i<nrProduse; i++){
         printf("%s %.2f %d ",p[i].nume,p[i].pret,p[i].nr_magazine);
@@ -81,6 +90,8 @@ void afisare(struct produs *p,int nrProduse){
     }
 }
 
+///Functie ce returneaza numele magazinului aflat la distanta cea
+/// mai mica fata de coordonatele date ca si argumente
 char *locatie(char *nume_produs,int coordonata_x,int coordonata_y,struct produs *p,int nrProduse){
     for (int i=0; i<nrProduse; i++){
         if (strcmp(nume_produs,p[i].nume)==0){
@@ -100,6 +111,7 @@ char *locatie(char *nume_produs,int coordonata_x,int coordonata_y,struct produs 
     }
 }
 
+///Functie comparatoare pentru functia qsort
 int cmp(const void* a,const void* b){
     struct produs p1 = *((struct produs*)a);
     struct produs p2 = *((struct produs*)b);
@@ -114,10 +126,12 @@ int cmp(const void* a,const void* b){
     return -1;
 }
 
+///Functia de sortare
 void sortare(struct produs **p,int nrProduse){
     qsort(*p,nrProduse,sizeof(struct produs),cmp);
 }
 
+///Functie ce elimina nodul din lista cu numele *nume*
 void eliminare(struct nod **L, char *nume){
     if (strcmp((*L)->prod.nume,nume)==0){
         struct nod *tinta = (*L);
@@ -142,6 +156,8 @@ int main() {
     printf("\n%s ",locatie("Mouse",43,65,p,nrProduse));
     sortare(&p,nrProduse);
     afisare(p,nrProduse);
+
+    ///Adauga elementele in lista
     for (int i=0; i<nrProduse; i++)
         adaugareElementeLista(&cap,p[i]);
     printf("\n\nLista:\n");
